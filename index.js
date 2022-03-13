2f580b446f3f96d3bd2fca310cf6a40dd5fb3f40
Daniil <traitnil@gmail.com>
2022-03-13 16:58:46 +0200

    switch

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 
 // const getMaxAndMin = function (numbe1, numbe2) {
 //   if (numbe1 > numbe2) {
-//     return numbe1;
+//     console.log (numbe1;
 //   } else {
-//     return numbe2;
+//     console.log (numbe2;
 //   }
 // };
 
@@ -17,7 +17,7 @@
 // const userNumber = Number(prompt("Введите число:"));
 
 // const isEven = function(num){
-//         return num % 2 === 0;
+//         console.log (num % 2 === 0;
 // }
 
 // const remainderofthenumber = isEven(userNumber);
@@ -29,7 +29,7 @@
 
 // const TimesOfDay = function(hour) {
 //   let theme = (hour >= 9 && 17 <= hour) ? 'light' : 'dark';
-// return theme;
+// console.log (theme;
 // }
 // const Times = TimesOfDay(userTimesOfDay);
 // alert(Times);
@@ -39,14 +39,14 @@
 // const hour = 12;
 // debugger;
 //   let theme = (hour >= 9 && 17 <= hour) ? 'light' : 'dark';
-//   console.log(theme);
+//   console.log((theme);
 
 // ------------
 
 // let number = Number(prompt("Введите число"));
 // const numbers = function (num) {
 //   let result = isNaN(num) ? "некоректное число" :  "все хорошо";
-//   return result;
+//   console.log (result;
 // };
 // const Times = numbers(number);
 // alert(Times);
@@ -56,21 +56,42 @@
 // let number = Number(prompt("Введите число:"));
 // const numbers = function (num) {
 //   if (isNaN(num)) {
-//     return "Некоректное число";
+//     console.log ("Некоректное число";
 //   } else if (num % 5 === 0) {
-//     return "делиться на 5";
+//     console.log ("делиться на 5";
 //   } else if (num % 3 === 0) {
-//     return "делиться на 3";
+//     console.log ("делиться на 3";
 //   } else if (num % 2 === 0) {
-//         return "делиться на 2";
+//         console.log ("делиться на 2";
 //   }else {
-//         return "не делиться не на 5 не на 3 не на 2!";
+//         console.log ("не делиться не на 5 не на 3 не на 2!";
 //   }
 // };
 // const Times = numbers(number);
 // alert(Times);
 
-
 // ------------
 
+let userInput = prompt(
+  "Выберите позицию меню: \n 1 - Чай \n 2 - Кофе \n 3 - Сок \n 4 - Лимонад \n 5 - Вода"
+);
 
+switch (userInput) {
+  case "1":
+    console.log("Вот ваш чай");
+    break;
+  case "2":
+    console.log("Вот ваш кофе");
+    break;
+  case "3":
+    console.log("Вот ваш сок");
+    break;
+  case "4":
+    console.log("Вот ваш лимонад");
+    break;
+  case "5":
+    console.log("Вот ваша вода");
+    break;
+  default:
+    console.log("Такого у нас нет");
+}
