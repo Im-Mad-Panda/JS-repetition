8c9c6f99e5a6e490b6dc5bddc9ddddd2e1de53bf
Daniil <traitnil@gmail.com>
2022-03-13 16:15:31 +0200

    operators-functions

---
+++ index.js
@@ -0,0 +1,76 @@
+// const userNumber = Number(prompt("Please, enter a number 1"));
+// const userNumber2 = Number(prompt("Please, enter a number 2"));
+
+// const getMaxAndMin = function (numbe1, numbe2) {
+//   if (numbe1 > numbe2) {
+//     return numbe1;
+//   } else {
+//     return numbe2;
+//   }
+// };
+
+// const numbers = getMaxAndMin(userNumber, userNumber2);
+// alert(numbers + ' - больше');
+
+// ------------
+
+// const userNumber = Number(prompt("Введите число:"));
+
+// const isEven = function(num){
+//         return num % 2 === 0;
+// }
+
+// const remainderofthenumber = isEven(userNumber);
+// alert(remainderofthenumber);
+
+// ------------
+
+// const userTimesOfDay = Number(prompt("Введите время суток:"));
+
+// const TimesOfDay = function(hour) {
+//   let theme = (hour >= 9 && 17 <= hour) ? 'light' : 'dark';
+// return theme;
+// }
+// const Times = TimesOfDay(userTimesOfDay);
+// alert(Times);
+
+// ------------
+
+// const hour = 12;
+// debugger;
+//   let theme = (hour >= 9 && 17 <= hour) ? 'light' : 'dark';
+//   console.log(theme);
+
+// ------------
+
+// let number = Number(prompt("Введите число"));
+// const numbers = function (num) {
+//   let result = isNaN(num) ? "некоректное число" :  "все хорошо";
+//   return result;
+// };
+// const Times = numbers(number);
+// alert(Times);
+
+// ------------
+
+// let number = Number(prompt("Введите число:"));
+// const numbers = function (num) {
+//   if (isNaN(num)) {
+//     return "Некоректное число";
+//   } else if (num % 5 === 0) {
+//     return "делиться на 5";
+//   } else if (num % 3 === 0) {
+//     return "делиться на 3";
+//   } else if (num % 2 === 0) {
+//         return "делиться на 2";
+//   }else {
+//         return "не делиться не на 5 не на 3 не на 2!";
+//   }
+// };
+// const Times = numbers(number);
+// alert(Times);
+
+
+// ------------
+
+
