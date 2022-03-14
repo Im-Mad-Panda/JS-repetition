dba68e682900ba7aea8d9025764864c32654221a
Daniil <traitnil@gmail.com>
2022-03-14 20:54:08 +0200

    HighOrderFunction

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let userName = prompt("Введите имя:");
-
-let getName = function(name) {
-  let theme = (name) ? 'Привет, ' + name : 'Привет Аноним!';
-  return theme;
+function sum(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function mult(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
 }
 
-const result = getName(userName);
-alert(result);
+// Callback - функция как арумент переданая другой функции.
+// Функция высшего порядка.
+
+
+function highOrderFunction(num1, num2, functionToPerform){
+  return functionToPerform(num1, num2);
+}
+
+const res = highOrderFunction(3, 4, sum);
+console.log(res);
