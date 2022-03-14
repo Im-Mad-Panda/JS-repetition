9bbcb912beed6963d5a603864d743a7715a9f640
Daniil <traitnil@gmail.com>
2022-03-14 23:16:41 +0200

    while

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-function sum(a, b) {
-  return a + b;
-}
-function sub(a, b) {
-  return a - b;
-}
-function mult(a, b) {
-  return a * b;
-}
-function div(a, b) {
-  return a / b;
-}
+// let count = 0;
 
-// Callback - функция как арумент переданая другой функции.
-// Функция высшего порядка.
+// while(count++ < 10){
+//   console.log(count)
+// }
 
 
-function highOrderFunction(num1, num2, functionToPerform){
-  return functionToPerform(num1, num2);
-}
+//-----------
 
-const res = highOrderFunction(3, 4, sum);
-console.log(res);
+
+const GOOD_PASSWORD = "qwerty123";
+
+let count = 0;
+const MAX_TRY = 5;
+
+while (count++ < MAX_TRY) {
+
+  let password = prompt("Введите пароль:");
+
+  if (password === GOOD_PASSWORD) {
+    break;
+  }
+}
+console.log("password good");
+
+
+//------------
+
+
+
+// while(count < 10){
+//   if(++count % 2 === 1){
+//     continue;
+//   }
+//   console.log(count);
+// }
+
+// console.log('END OF SCRIPT');
