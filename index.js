e02817ea4280b27d3463525f791cc8376afd5b7f
Daniil <traitnil@gmail.com>
2022-03-15 20:50:17 +0200

    ForLoop

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
 //   console.log(count)
 // }
 
-
 //-----------
-
 
 // const GOOD_PASSWORD = "qwerty123";
 
@@ -23,17 +21,32 @@
 // }
 // console.log("password good");
 
+//------------
+
+// let count = 0;
+// const MAX_TRY = 5;
+
+// do{
+//   console.log('Do' + count);
+// }while(count++ < MAX_TRY)
+
+// console.log("password good");
 
 //------------
 
-let count = 0;
-const MAX_TRY = 5;
+// for(инициализация; условие; изменение на каждом шаге итерации){
 
-do{
-  console.log('Do' + count);
-}while(count++ < MAX_TRY)
+// }
 
-console.log("password good");
+// const GOOD_PASSWORD = "qwerty123";
+// const MAX_TRY = 5;
+// for (i = 0; i < MAX_TRY; i++) {
+//     let password = prompt("Введите пароль:");
+//     if (password === GOOD_PASSWORD) {
+//         break;
+//     }
+// }
+// console.log("password good");
 
 //------------
 
@@ -45,3 +58,18 @@ console.log("password good");
 // }
 
 // console.log('END OF SCRIPT');
+
+//------------
+
+function factorial(a) {
+
+let result = 1;
+
+  for (i = 1; i <= a; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+const res = factorial(5);
+console.log(res);
