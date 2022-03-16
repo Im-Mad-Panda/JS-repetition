49752133a05b70bc71e15e4fef8388a6ad156b3f
Daniil <traitnil@gmail.com>
2022-03-16 18:02:54 +0200

    sumEvenNumbers

--- index.js
+++ index.js
@@ -1,75 +1,12 @@
-// let count = 0;
+function sumEvenNumbers(from, to) {
+  let sum = 0;
 
-// while(count++ < 10){
-//   console.log(count)
-// }
-
-//-----------
-
-// const GOOD_PASSWORD = "qwerty123";
-
-// let count = 0;
-// const MAX_TRY = 5;
-
-// while (count++ < MAX_TRY) {
-
-//   let password = prompt("Введите пароль:");
-
-//   if (password === GOOD_PASSWORD) {
-//     break;
-//   }
-// }
-// console.log("password good");
-
-//------------
-
-// let count = 0;
-// const MAX_TRY = 5;
-
-// do{
-//   console.log('Do' + count);
-// }while(count++ < MAX_TRY)
-
-// console.log("password good");
-
-//------------
-
-// for(инициализация; условие; изменение на каждом шаге итерации){
-
-// }
-
-// const GOOD_PASSWORD = "qwerty123";
-// const MAX_TRY = 5;
-// for (i = 0; i < MAX_TRY; i++) {
-//     let password = prompt("Введите пароль:");
-//     if (password === GOOD_PASSWORD) {
-//         break;
-//     }
-// }
-// console.log("password good");
-
-//------------
-
-// while(count < 10){
-//   if(++count % 2 === 1){
-//     continue;
-//   }
-//   console.log(count);
-// }
-
-// console.log('END OF SCRIPT');
-
-//------------
-
-function factorial(a) {
-
-let result = 1;
-
-  for (i = 1; i <= a; i++) {
-    result = result * i;
+  for (let i = from; i <= to; i++) {
+    if (i % 2 === 0) {
+      sum += i; // sum = sum + i
+    }
   }
-  return result;
+  return sum;
 }
-
-const res = factorial(5);
+let res = sumEvenNumbers(4,10);
 console.log(res);
