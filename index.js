34807330a8fa7c215f9193a2b06314fa5b83209e
Daniil <traitnil@gmail.com>
2022-03-16 18:46:12 +0200

    factorialBugFixing

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function sumEvenNumbers(from, to) {
-  let sum = 0;
+function factorial(number) {
 
-  for (let i = from; i <= to; i++) {
-    if (i % 2 === 0) {
-      sum += i; // sum = sum + i
-    }
-  }
-  return sum;
+if(isNaN(number)){
+  return false
 }
-let res = sumEvenNumbers(4,10);
+
+if(number === 1 || number === 0){
+  return 1;
+}
+
+const MAX_NUM = 250;
+
+if(number > MAX_NUM){
+  return false;
+}
+
+  let result = 1n;
+
+  for (let i = 1; i <= number; i++) {
+      result *= BigInt(i); // result = result * i
+  }
+  return result;
+}
+let res = factorial(20);
 console.log(res);
