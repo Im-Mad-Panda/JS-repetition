9404bb80798eca1d2b9e7d5132fa7ea51dc81092
Daniil <traitnil@gmail.com>
2022-03-17 23:12:25 +0200

    carTaskResolve

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-function User(name, surname, fullName, weight, age){
+function Car(name, maxSpeed) {
   this.name = name;
-  this.surname = surname;
-  this.fullName = fullName;
-  this.weight = weight;
-  this.age = age;
+  this.maxSpeed = maxSpeed;
+  this.speed = 0;
+
+  this.accelerate = function (value) {
+    if (value < 0) {
+      return false;
+    }
+    this.speed = this.speed + value;
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+      return this.speed;
+    }
+    return this.speed;
+  };
+
+  this.deaccelerate = function (value) {
+    if (value < 0) {
+      return false;
+    }
+    this.speed = this.speed - value;
+    if (this.speed < 0) {
+      this.speed = 0;
+      return this.speed;
+    }
+    return this.speed;
+  }
+
+  this.stop = function () {
+    return (this.speed = 0)
+  }
 }
 
-const user = new User("Марк", "Ray", "Ведмедев Марк Сергеевич", 75, 30);
-console.log(user);
+const blueCar = new Car("VW", 300);
+console.log(blueCar);
