4e3481c9a68a96259d3c9788308eca312ff8f37d
Daniil <traitnil@gmail.com>
2022-03-17 21:07:33 +0200

    objectUser

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-const cat = {
-  name: "Barsik",
-  age: 4,
-  color: "ginger",
-  weight: 5,
-  breed: "Cheshirkiy",
-  isNale: true,
-  isSleeping: false,
-  run: function () { //run - метод
-    return "I am running";
-  },
-  sleep: function () {
-    return "Zzzz...";
-  },
-  meow: function () {
-    return "Meowww!";
-  },
-};
+function User(name, surname, fullName, weight, age){
+  this.name = name;
+  this.surname = surname;
+  this.fullName = fullName;
+  this.weight = weight;
+  this.age = age;
+}
 
-const res = cat.run(); // вызов функции
-alert(res);
-
-cat.age = cat.age + 1; //cat.age++
-
-alert(cat.age); // read
-
-delete cat.isSleeping; // delete
-
-// cat.isSleeping = undefined;
-
-cat.girlFriend = "Murka";
+const user = new User("Марк", "Ray", "Ведмедев Марк Сергеевич", 75, 30);
+console.log(user);
