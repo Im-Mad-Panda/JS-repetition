034aece0a620bc7f5f371e58f16ae269326b922d
Daniil <traitnil@gmail.com>
2022-03-19 23:39:33 +0200

    multypTable

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-function Cat(name, age, breed, color, isMale){
-  this.name = name;
-  this.age = age;
-  this.breed = breed;
-  this.color = color;
-  this.isMale = isMale;
+function getMultypTable() {
+  const table = {};
+  for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+      table[`${i} * ${j}`] = i * j;
+    }
+  }
+  return table;
 }
-
-const cat1 = new Cat('Barsik', 5, 'British', 'blue', true);
-const cat2 = new Cat('Murka', 3, 'British', 'blue', false);
-
-
-cat1.girlfriend = cat2;
-
-cat1.girlfriend.name // или cat1.girlfriend['name']
+getMultypTable();
+console.log(getMultypTable());
