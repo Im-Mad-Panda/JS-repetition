1ae6611f5493d30a28792dbad84ad27489ae7cdf
Daniil <traitnil@gmail.com>
2022-03-19 22:17:34 +0200

    catWeddingDay

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const user = {
-  "favorite color": "green",
-  0: "test",
-};
-
-user.favoriteColor; // Доступ к свойству
-
-user["favorite color"]; //доступ к свойству с возможностью вычисления
-
-user[0];
-user["0"];
-
-const test = "favorite color";
-
-user[test];
-
-const weekPlan = {
-  Mon: "Visit to stomatologist",
-  Tue: "Got salary",
-  Wed: "Meet friends",
-  Thu: "Hard working",
-  Fri: "Free day",
-  Sat: "Go to party",
-};
-
-function getPlanning() {
-  const dayWeek = prompt(
-    "Выберите день недели: \nMon - for monday \nTue - Thuesday \nWed - Wednesday \nThu - Thuesday \nFri - Friday \nSat - Saturday"
-  );
-  alert(weekPlan[dayWeek] ? weekPlan[dayWeek] : 'Nothing planned for this day' );
+function Cat(name, age, breed, color, isMale){
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+  this.color = color;
+  this.isMale = isMale;
 }
-getPlanning();
+
+const cat1 = new Cat('Barsik', 5, 'British', 'blue', true);
+const cat2 = new Cat('Murka', 3, 'British', 'blue', false);
+
+
+cat1.girlfriend = cat2;
+
+cat1.girlfriend.name // или cat1.girlfriend['name']
