de4acd25bc7be082ec97d173c0bea0ad4516249e
Daniil <traitnil@gmail.com>
2022-03-19 22:08:24 +0200

    accessingTheCalculationProperty

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const PROTOCOL = 'http://';
-const SERVER_IP = 'www.google.com';
-const PROT = '80';
+const user = {
+  "favorite color": "green",
+  0: "test",
+};
 
-function createQuery(absPath){
-  const url = `${PROTOCOL}${SERVER_IP}:${PROT}${absPath}`;
-  return url;
+user.favoriteColor; // Доступ к свойству
+
+user["favorite color"]; //доступ к свойству с возможностью вычисления
+
+user[0];
+user["0"];
+
+const test = "favorite color";
+
+user[test];
+
+const weekPlan = {
+  Mon: "Visit to stomatologist",
+  Tue: "Got salary",
+  Wed: "Meet friends",
+  Thu: "Hard working",
+  Fri: "Free day",
+  Sat: "Go to party",
+};
+
+function getPlanning() {
+  const dayWeek = prompt(
+    "Выберите день недели: \nMon - for monday \nTue - Thuesday \nWed - Wednesday \nThu - Thuesday \nFri - Friday \nSat - Saturday"
+  );
+  alert(weekPlan[dayWeek] ? weekPlan[dayWeek] : 'Nothing planned for this day' );
 }
-createQuery();
-console.log(createQuery('/index.html'))
+getPlanning();
